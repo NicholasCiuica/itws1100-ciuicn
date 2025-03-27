8b74f8dc87e9b42730297606dcf466740884fb64
NicholasCiuica <nick.ciuica@gmail.com>
2025-03-27 13:24:53 -0400

    comments, readme, etc.

---
+++ classes/IntroITWS/labs/lab08/README.md
@@ -0,0 +1,5 @@
+In this lab, I used AJAX to dynamically generate a list of lab links in my labs page.
+
+I have a JSON file within the lab 8 folder that is read using JS file in the same folder using AJAX. This JS file then makes a DOM object using JQuery and the info from the JSON file, which is inserted into a content div in the labs HTML page.
+
+I used JQuery to add some customizable tooltips to content blocks in labs.html and lab08/index.html.
--- classes/IntroITWS/labs/lab08/script.js
+++ classes/IntroITWS/labs/lab08/script.js
@@ -5,12 +5,14 @@ $(document).ready(function() {
     dataType: "json",
     success: function(responseData, status) {
       var pageDOM = '<p>' + responseData.text + '</p><ul>';
+      //for each lab, make a link to that lab within the lab list
       $.each(responseData.labs, function(i, lab) {
         pageDOM += '<li><a href="' + lab.link + '">';
         pageDOM += lab.title + ' (' + lab.desc + ')';
         pageDOM += '</a></li>';
       });
       pageDOM += '</ul>';
+      //#page-content is an empty div for the lab page's main content
       $("#page-content").html(pageDOM);
       
     }, error: function(msg) {
