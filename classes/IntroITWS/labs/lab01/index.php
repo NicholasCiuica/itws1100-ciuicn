3ec49595d3578816b9352e2d3a4cac8bb1748c45
NicholasCiuica <nick.ciuica@gmail.com>
2025-04-08 13:00:10 -0400

    trying to convert lab 1 to php

---
+++ classes/IntroITWS/labs/lab01/index.php
@@ -0,0 +1,34 @@
+<?php
+  include("../../../quiz3/includes/head.inc.php");
+?>
+
+<header>
+  <a href="/iit/"><h3>Nick's Personal Website</h3></a>
+  <h1>Lab 1</h1>
+  <p>Intro to ITWS, Labs</p>
+</header>
+
+<?php
+  include("../../../quiz3/includes/nav.inc.php");
+?>
+
+<div class="block">
+    <p>
+      In lab 1 I set up my Azure server and learned how to host a website on this server by pulling from GitHub.
+    </p>
+</div>
+
+<div class="block">
+    <p>
+      Links to screenshots taken as I was completing the lab:<br><br>
+      <a href="ciuicn-lab1/ciuicn-lab1-AzureStatus.png" target="_blank">ciuicn-lab1-AzureStatus.png</a><br>
+      <a href="ciuicn-lab1/ciuicn-lab1-checkphp.png" target="_blank">ciuicn-lab1-checkphp.png</a><br>
+      <a href="ciuicn-lab1/ciuicn-lab1-GitHubInfo.png" target="_blank">ciuicn-lab1-GitHubInfo.png</a><br>
+      <a href="ciuicn-lab1/ciuicn-lab1-HelloWorld.png" target="_blank">ciuicn-lab1-HelloWorld.png</a><br>
+      <a href="ciuicn-lab1/ciuicn-lab1-PHPMyAdmin.png" target="_blank">ciuicn-lab1-PHPMyAdmin.png</a>
+    </p>
+</div>
+
+<?php
+  include("../../../quiz3/includes/foot.inc.php");
+?>
