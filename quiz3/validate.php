232d309fef143a2aeb5957ac73e5bd0e9f060ceb
NicholasCiuica <nick.ciuica@gmail.com>
2025-04-08 18:19:02 -0400

    query debugging

--- quiz3/validate.php
+++ quiz3/validate.php
@@ -5,11 +5,15 @@
   if ($db->connect_error) {
     echo 'Could not connect to the database. Error:' . $db->connect_error ;
   }
-
-  // $query = 'SELECT * FROM `mySiteUsers`';
-  // $result = $db->query($query);
-  // $numRecords = $result->num_rows;
   
   $loginUser = $_POST["user"];
   $loginPass = $_POST["pass"];
+
+  //These sources helped me formulate my SQL query
+    //https://stackoverflow.com/questions/4253960/sql-how-to-properly-check-if-a-record-exists
+    //https://stackoverflow.com/questions/11784289/does-it-make-sense-to-use-limit-1-in-a-query-select-1
+  $query = 'SELECT * FROM mySiteUsers WHERE user = ' . $loginUser . ' AND pass = ' . $loginPass . ' LIMIT 1';
+  $result = $db->query($query);
+  $numRecords = $result->num_rows;
+  echo $numRecords;
 ?>
